2dd409d3bac6b48e179e0445792faa3fa670217e
Andres6936 <joan.buritica@artemisa.tech>
2022-09-04 18:30:26 -0500

    Fixed problem in code

--- Source/Biome.py
+++ Source/Biome.py
@@ -6,7 +6,7 @@ from Source.Context import WORLD_WIDTH, WORLD_HEIGHT, SCREEN_HEIGHT
 def BiomeMap(Chars, Colors):
     for x in range(WORLD_WIDTH):
         for y in range(WORLD_HEIGHT):
-            libtcod.console_put_char_ex(0, x, y + SCREEN_HEIGHT / 2 - WORLD_HEIGHT / 2, Chars[x][y], Colors[x][y],
+            libtcod.console_put_char_ex(0, x, y + SCREEN_HEIGHT // 2 - WORLD_HEIGHT // 2, Chars[x][y], Colors[x][y],
                                         libtcod.black)
 
     libtcod.console_flush()
