f1aab52976f3f3a91c26d52b7010cd18bf3e7f78
Andres6936 <joan.buritica@artemisa.tech>
2023-01-15 18:14:12 -0500

    Prepare Scene Manager

--- Source/Scene/SceneManager.py
+++ Source/Scene/SceneManager.py
@@ -1,15 +1,22 @@
+from Source.Scene.IScene import IScene
+from Source.Scene.SceneMain import SceneMain
+
+
 class SceneManager:
     def __init__(self):
-        self.running = True
+        self.running: bool = True
+        self.mainScene: IScene = SceneMain()
+        # Point to current scene to render
+        self.currentScene: IScene = self.mainScene
 
     def isRunning(self):
         return self.running
 
     def events(self):
-        pass
+        self.currentScene.events()
 
     def clear(self):
-        pass
+        self.currentScene.clear()
 
     def draw(self):
-        pass
+        self.currentScene.draw()
