2f8bfc4bf3af1e771ad20889cd17d3b46ed0fb6c
Andres6936 <joan.buritica@artemisa.tech>
2023-01-14 19:22:23 -0500

    Use of global API

--- Source/Biome.py
+++ Source/Biome.py
@@ -7,7 +7,12 @@ from Source.Context import SCREEN_HEIGHT, WORLD_HEIGHT, WORLD_WIDTH
 def BiomeMap(Chars, Colors, console: Console):
     for x in range(WORLD_WIDTH):
         for y in range(WORLD_HEIGHT):
-            console.print(x, y + SCREEN_HEIGHT // 2 - WORLD_HEIGHT // 2, str(Chars[x][y]), Colors[x][y], libtcod.black)
+            glyph = Chars[x][y]
+            if not isinstance(glyph, int):
+                glyph = ord(glyph)
+            glyph = libtcod.tileset.CHARMAP_CP437[glyph]  # Converts from EASCII to Unicode.
+            libtcod.console_put_char_ex(console, x, y + SCREEN_HEIGHT // 2 - WORLD_HEIGHT // 2, glyph, Colors[x][y],
+                                        libtcod.black)
             libtcod.console_put_char_ex(0, x, y + SCREEN_HEIGHT // 2 - WORLD_HEIGHT // 2, Chars[x][y], Colors[x][y],
                                         libtcod.black)
 
