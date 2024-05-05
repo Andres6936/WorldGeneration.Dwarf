8a2a480479da0f281cf583fbcaf8ad535517f564
Andres6936 <andres6936@gmail.com>
2024-05-05 18:10:29 -0500

    Refactor Heightmap class and update related tests
    Updated Heightmap class to include new methods for creating heightmaps, adding hills, and retrieving values. Modified the tests to align with these changes. The refactoring also improved the flexibility of the Heightmap class and overall readability of the code.

--- Tytonic/assembly/index.ts
+++ Tytonic/assembly/index.ts
@@ -11,21 +11,31 @@ class Heightmap {
     this.height = height;
   }
 
-  getValues(): Float32Array {
+  public static from(target: Float32Array, width: i32, height: i32): Heightmap {
+    const heightmap = new Heightmap(width, height);
+    heightmap.setValue(target);
+    return heightmap;
+  }
+
+  private setValue(target: Float32Array) : void {
+    this.values.set(target);
+  }
+
+  public getValues(): Float32Array {
     return this.values;
   }
 
-  addHill(hx: f32, hy: f32, h_radius: f32, h_height: f32): void {
+  public addHill(hx: f32, hy: f32, h_radius: f32, h_height: f32): void {
     const h_radius2: f32 = h_radius * h_radius;
     const coef: f32 = h_height / h_radius2;
-    const minx: i32 = Math.max((hx - h_radius), 0);
-    const miny: i32 = Math.max((hy - h_radius), 0);
-    const maxx: i32 = Math.min(Math.ceil(hx + h_radius), this.width);
-    const maxy: i32 = Math.min(Math.ceil(hy + h_radius), this.height);
+    const minx: i32 = Math.max((hx - h_radius), 0) as i32;
+    const miny: i32 = Math.max((hy - h_radius), 0) as i32;
+    const maxx: i32 = Math.min(Math.ceil(hx + h_radius), this.width) as i32;
+    const maxy: i32 = Math.min(Math.ceil(hy + h_radius), this.height) as i32;
     for (let y: i32 = miny; y < maxy; y++) {
-      const y_dist: f32 = (y - hy) * (y - hy);
+      const y_dist: f32 = (y as f32 - hy) * (y as f32 - hy);
       for (let x: i32 = minx; x < maxx; x++) {
-        const x_dist: f32 = (x - hx) * (x - hx);
+        const x_dist: f32 = (x as f32 - hx) * (x as f32 - hx);
         const z: f32 = h_radius2 - x_dist - y_dist;
         if (z > 0) {
           this.values[x + y * this.width] += z * coef;
@@ -35,6 +45,14 @@ class Heightmap {
   }
 }
 
-export function get(width: i32, height: i32): Float32Array {
-  return new Heightmap(width, height).getValues();
+export function newHeightmap(width: i32, height: i32): Heightmap {
+  return new Heightmap(width, height);
+}
+
+export function addHillToHeightmap(target: Heightmap, hx: f32, hy: f32, h_radius: f32, h_height: f32): void {
+  target.addHill(hx, hy, h_radius, h_height);
+}
+
+export function getValuesOfHeightmap(target: Heightmap): Float32Array {
+  return target.getValues();
 }
--- Tytonic/tests/index.ts
+++ Tytonic/tests/index.ts
@@ -1,5 +1,9 @@
 import assert from "assert";
-import { get } from "../build/debug.js";
-assert.strictEqual(get(10, 10).length, 100);
-console.log("ok");
-console.log(get(10, 10))
+import { newHeightmap, addHillToHeightmap, getValuesOfHeightmap } from "../build/debug.js";
+// assert.strictEqual(get(10, 10).length, 100);
+
+const heightmap = newHeightmap(80, 50);
+addHillToHeightmap(heightmap, 40, 25, 12, 6);
+const valuesOf = getValuesOfHeightmap(heightmap);
+
+console.log(valuesOf)
