07138c489b0aa6f69acd6d83e83ac45591e04bcf
Andres6936 <andres6936@gmail.com>
2024-06-01 16:13:42 -0500

    Refactored Heightmap class and moved export functions
    In the assembly/heightmap.ts file, the Heightmap class was refactored to be private and a new set of export functions were added. Meanwhile, the redundant export functions in the assembly/index.ts file were removed and replaced with a direct export from "./heightmap".

--- Tytonic/assembly/heightmap.ts
+++ Tytonic/assembly/heightmap.ts
@@ -1,6 +1,6 @@
 import {ImmutableTuple} from "./immutable-tuple";
 
-export class Heightmap {
+class Heightmap {
   private readonly width: i32;
   private readonly height: i32;
   private readonly values: Float32Array;
@@ -85,3 +85,18 @@ export class Heightmap {
   }
 }
 
+export function newHeightmap(width: i32, height: i32): Heightmap {
+  return new Heightmap(width, height);
+}
+
+export function addHillToHeightmap(target: Heightmap, hx: f32, hy: f32, h_radius: f32, h_height: f32): void {
+  target.addHill(hx, hy, h_radius, h_height);
+}
+
+export function normalizeHeightmap(target: Heightmap, min: f32, max: f32): void {
+  target.normalize(min, max);
+}
+
+export function getValuesOfHeightmap(target: Heightmap): Float32Array {
+  return target.getValues();
+}
--- Tytonic/assembly/index.ts
+++ Tytonic/assembly/index.ts
@@ -1,18 +1,2 @@
 // The entry file of your WebAssembly module.
-import {Heightmap} from "./heightmap";
-
-export function newHeightmap(width: i32, height: i32): Heightmap {
-  return new Heightmap(width, height);
-}
-
-export function addHillToHeightmap(target: Heightmap, hx: f32, hy: f32, h_radius: f32, h_height: f32): void {
-  target.addHill(hx, hy, h_radius, h_height);
-}
-
-export function normalizeHeightmap(target: Heightmap, min: f32, max: f32): void {
-  target.normalize(min, max);
-}
-
-export function getValuesOfHeightmap(target: Heightmap): Float32Array {
-  return target.getValues();
-}
+export * from "./heightmap";
