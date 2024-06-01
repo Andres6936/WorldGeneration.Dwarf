800e1283b8b5c4dc4428e4f65a47624ec0f89642
Andres6936 <andres6936@gmail.com>
2024-06-01 15:43:41 -0500

    Add normalize function and ImmutableTuple class to Heightmap
    The normalization function adjusts the values in the Heightmap to a new range, defined by the minimum and maximum values passed to it. The ImmutableTuple class allows the function to return both the current minimum and maximum values of the Heightmap. Added inBounds method for checking permissible boundaries.

--- Tytonic/assembly/index.ts
+++ Tytonic/assembly/index.ts
@@ -1,5 +1,10 @@
 // The entry file of your WebAssembly module.
 
+class ImmutableTuple<T> {
+  constructor(public readonly first: T, public readonly second: T) {
+  }
+}
+
 class Heightmap {
   private readonly width: i32;
   private readonly height: i32;
@@ -43,6 +48,46 @@ class Heightmap {
       }
     }
   }
+
+  public normalize(min: f32, max: f32): void {
+    const currentValuesOf = this.getMinMax();
+    const currentMin = currentValuesOf.first;
+    const currentMax = currentValuesOf.second;
+
+    if (currentMax - currentMin < f32.EPSILON) {
+      for (let i = 0; i != this.width * this.height; ++i) {
+        this.values[i] = min;
+      }
+    } else {
+      const normalizeScale: f32 = (max - min) / (currentMax - currentMin);
+      for (let i = 0; i != this.width * this.height ; ++i) {
+        this.values[i] = min + (this.values.at(i) - currentMin) * normalizeScale;
+      }
+    }
+  }
+
+  public getMinMax(): ImmutableTuple<f32> {
+    if (!this.inBounds(0, 0)) {
+      return new ImmutableTuple<f32>(0, 0);
+    }
+
+    let min: f32 = this.values.at(0);
+    let max: f32 = this.values.at(0);
+
+    for (let i = 0; i != this.width * this.height; i++) {
+      const value = this.values.at(i);
+      min = Math.min(min, value) as f32;
+      max = Math.max(max, value) as f32;
+    }
+
+    return new ImmutableTuple<f32>(min, max);
+  }
+
+  public inBounds(x: i32, y: i32): boolean {
+    if (x < 0 || x >= this.width) return false;
+    if (y < 0 || y >= this.height) return false;
+    return true;
+  }
 }
 
 export function newHeightmap(width: i32, height: i32): Heightmap {
@@ -51,6 +96,10 @@ export function newHeightmap(width: i32, height: i32): Heightmap {
 
 export function addHillToHeightmap(target: Heightmap, hx: f32, hy: f32, h_radius: f32, h_height: f32): void {
   target.addHill(hx, hy, h_radius, h_height);
+}
+
+export function normalizeHeightmap(target: Heightmap, min: f32, max: f32): void {
+  target.normalize(min, max);
 }
 
 export function getValuesOfHeightmap(target: Heightmap): Float32Array {
