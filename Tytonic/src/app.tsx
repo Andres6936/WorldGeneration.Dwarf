f54a8371b380b11b300b5b9c74c4423ee5ae81ad
Andres6936 <andres6936@gmail.com>
2024-05-31 19:29:00 -0500

    Add heatmap visualization and improve CSS defaults
    Updated the project to include a heatmap visualization using visx. This involved generating mock data, and defining scales and accessors for the heatmap. In addition, the default CSS for the project was improved, including the box-sizing model, margin defaults, built-in form typography styles, and more. New visx dependencies were added to the package.json. In the main.js file, a ParentSize wrapper was added to the App for responsive handling of width and height.

--- Tytonic/src/app.tsx
+++ Tytonic/src/app.tsx
@@ -1,5 +1,108 @@
-export default function () {
-    return (
-        <h1>Hello Dwarf</h1>
-    )
+import React from 'react';
+import {Group} from '@visx/group';
+import genBins, {type Bin, type Bins} from '@visx/mock-data/lib/generators/genBins';
+import {scaleLinear} from '@visx/scale';
+import {HeatmapRect} from '@visx/heatmap';
+import {getSeededRandom} from '@visx/mock-data';
+
+const cool1 = 'red';
+const cool2 = 'yellow';
+export const background = '#28272c';
+
+const seededRandom = getSeededRandom(0.41);
+
+const binData = genBins(
+  /* length = */ 80,
+  /* height = */ 50,
+  /** binFunc */ (idx) => 150 * idx,
+  /** countFunc */ (i, number) => 25 * (number - i) * seededRandom(),
+);
+
+function max<Datum>(data: Datum[], value: (d: Datum) => number): number {
+  return Math.max(...data.map(value));
+}
+
+// accessors
+const bins = (d: Bins) => d.bins;
+const count = (d: Bin) => d.count;
+
+const colorMax = max(binData, (d) => max(bins(d), count));
+const bucketSizeMax = max(binData, (d) => bins(d).length);
+
+// scales
+const xScale = scaleLinear<number>({
+  domain: [0, binData.length],
+});
+const yScale = scaleLinear<number>({
+  domain: [0, bucketSizeMax],
+});
+const rectColorScale = scaleLinear<string>({
+  range: [cool1, cool2],
+  domain: [0, colorMax],
+});
+
+export type HeatmapProps = {
+  width: number;
+  height: number;
+  margin?: { top: number; right: number; bottom: number; left: number };
+  separation?: number;
+  events?: boolean;
+};
+
+const defaultMargin = { top: 20, left: 20, right: 20, bottom: 110 };
+
+export default function App({
+  width,
+  height,
+  events = false,
+  margin = defaultMargin,
+}: HeatmapProps) {
+  // bounds
+  const size =
+    width > margin.left + margin.right ? width - margin.left - margin.right : width;
+  const xMax = size / 1.5;
+  const yMax = height - margin.bottom - margin.top;
+
+  const binWidth = xMax / binData.length;
+
+  xScale.range([0, xMax]);
+  yScale.range([yMax, 0]);
+
+  return (
+    <svg width={width} height={height}>
+      <rect x={0} y={0} width={width} height={height} fill={background} />
+      <Group top={margin.top} left={margin.left}>
+        <HeatmapRect
+          data={binData}
+          xScale={(d) => xScale(d) ?? 0}
+          yScale={(d) => yScale(d) ?? 0}
+          colorScale={rectColorScale}
+          binWidth={binWidth}
+          binHeight={binWidth}
+        >
+          {(heatmap) =>
+            heatmap.map((heatmapBins) =>
+              heatmapBins.map((bin) => (
+                <rect
+                  key={`heatmap-rect-${bin.row}-${bin.column}`}
+                  className="visx-heatmap-rect"
+                  width={bin.width}
+                  height={bin.height}
+                  x={bin.x}
+                  y={bin.y}
+                  fill={bin.color}
+                  fillOpacity={bin.opacity}
+                  onClick={() => {
+                    if (!events) return;
+                    const { row, column } = bin;
+                    alert(JSON.stringify({ row, column, bin: bin.bin }));
+                  }}
+                />
+              )),
+            )
+          }
+        </HeatmapRect>
+      </Group>
+    </svg>
+  );
 }
--- Tytonic/src/main.tsx
+++ Tytonic/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import ReactDOM from 'react-dom/client'
 import App from './app.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ParentSize>
+            {({width, height}) => <App width={width} height={height}/>}
+        </ParentSize>
+    </React.StrictMode>,
 )
