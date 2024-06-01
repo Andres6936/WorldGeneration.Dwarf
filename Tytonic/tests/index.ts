83cf0440e0622f449b3ff28df8621f12a4dc9a9a
Andres6936 <andres6936@gmail.com>
2024-06-01 11:34:42 -0500

    Update tsconfig and improve tests for heightmap
    Updated the tsconfig.json file to include the "tests" directory and tweaked the relative path. In tests/index.ts, the size of the heightmap was reduced and console logging was added to improve testing visibility. This ensures better monitoring of heightmap functionality.

--- Tytonic/tests/index.ts
+++ Tytonic/tests/index.ts
@@ -1,9 +1,21 @@
-import assert from "assert";
 import { newHeightmap, addHillToHeightmap, getValuesOfHeightmap } from "../build/debug.js";
-// assert.strictEqual(get(10, 10).length, 100);
+import {ReadonlyArray2D} from "../src/array2d.ts";
+import * as console from "node:console";
 
-const heightmap = newHeightmap(80, 50);
-addHillToHeightmap(heightmap, 40, 25, 12, 6);
+const MAP_WIDTH = 3;
+const MAP_HEIGHT = 3;
+
+const heightmap = newHeightmap(MAP_WIDTH, MAP_HEIGHT);
+// addHillToHeightmap(heightmap, 40, 25, 12, 6);
 const valuesOf = getValuesOfHeightmap(heightmap);
 
-console.log(valuesOf)
+const heightmapOf = new ReadonlyArray2D(valuesOf, MAP_WIDTH, MAP_HEIGHT);
+// for (let heightmapOfElement of heightmapOf) {
+//     console.log(heightmapOfElement)
+// }
+//
+// console.log("Using @{map} method of ReadonlyArray2D")
+
+heightmapOf.map((valueOf) => {
+    console.log(valueOf);
+})
