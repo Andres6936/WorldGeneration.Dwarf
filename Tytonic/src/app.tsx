6f2cbe59ec743954bb5dc2106739c71148fabdb2
Andres6936 <andres6936@gmail.com>
2024-06-01 11:55:30 -0500

    Replaced '@visx' library with 'd3' for heatmap generation
    The dependencies and code related to the '@visx' library used for heatmap generation were removed. Instead, the 'd3' library was added as a dependency and used to generate the heatmap. Additionally, code was added for improved labeling and rectangle building.

--- Tytonic/src/app.tsx
+++ Tytonic/src/app.tsx
@@ -1,139 +1,131 @@
-import React from 'react';
-import {Group} from '@visx/group';
-import genBins, {type Bin, type Bins} from '@visx/mock-data/lib/generators/genBins';
-import {scaleLinear} from '@visx/scale';
-import {HeatmapRect} from '@visx/heatmap';
-import {getSeededRandom} from '@visx/mock-data';
-import {newHeightmap, addHillToHeightmap, getValuesOfHeightmap} from "tytonic";
-import {ReadonlyArray2D} from "./array2d.ts";
+import type {ReadonlyArray2D} from "./array2d.ts";
+import {useMemo} from "react";
+import * as d3 from "d3"
 
-const cool1 = 'red';
-const cool2 = 'yellow';
-export const background = '#28272c';
 
-const seededRandom = getSeededRandom(0.41);
-
-const binData = genBins(
-  /* length = */ 80,
-  /* height = */ 50,
-  /** binFunc */ (idx) => 150 * idx,
-  /** countFunc */ (i, number) => 25 * (number - i) * seededRandom(),
-);
-
-function max<Datum>(data: Datum[], value: (d: Datum) => number): number {
-  return Math.max(...data.map(value));
-}
-
-// accessors
-const bins = (d: Bins) => d.bins;
-const count = (d: Bin) => d.count;
-
-const colorMax = max(binData, (d) => max(bins(d), count));
-const bucketSizeMax = max(binData, (d) => bins(d).length);
-
-// scales
-const xScale = scaleLinear<number>({
-  domain: [0, binData.length],
-});
-const yScale = scaleLinear<number>({
-  domain: [0, bucketSizeMax],
-});
-const rectColorScale = scaleLinear<string>({
-  range: [cool1, cool2],
-  domain: [0, colorMax],
-});
+const MARGIN = {top: 10, right: 50, bottom: 30, left: 50};
 
 export type HeatmapProps = {
-  width: number;
-  height: number;
-  margin?: { top: number; right: number; bottom: number; left: number };
-  separation?: number;
-  events?: boolean;
+    width: number;
+    height: number;
+    valueOf: ReadonlyArray2D;
 };
 
-const defaultMargin = { top: 20, left: 20, right: 20, bottom: 110 };
-
-const WORLD_WIDTH = 10;
-const WORLD_HEIGHT = 10;
-
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * The value is no lower than min (or the next integer greater than min
- * if min isn't an integer) and no greater than max (or the next integer
- * lower than max if max isn't an integer).
- * Using Math.round() will give you a non-uniform distribution!
- *
- * Ref: https://stackoverflow.com/a/1527820
- */
-function getRandomInt(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const heightmap = newHeightmap(WORLD_WIDTH, WORLD_HEIGHT);
-
-// for (let i = 0; i < 250; i++) {
-//     addHillToHeightmap(heightmap,
-//         getRandomInt(WORLD_WIDTH / 10, WORLD_WIDTH - WORLD_WIDTH / 10),
-//         getRandomInt(WORLD_HEIGHT / 10, WORLD_HEIGHT - WORLD_HEIGHT / 10),
-//         getRandomInt(12, 16),
-//         getRandomInt(6, 10)
-//     );
-// }
-
-const valuesOf = getValuesOfHeightmap(heightmap);
-console.log(valuesOf)
-
-const heightmapOf = new ReadonlyArray2D(valuesOf, WORLD_WIDTH, WORLD_HEIGHT);
-// for (let heightmapValueOf of heightmapOf) {
-//     console.log(heightmapValueOf)
-// }
-
 export default function App({
-  width,
-  height,
-  margin = defaultMargin,
-}: HeatmapProps) {
-  // bounds
-  const size =
-    width > margin.left + margin.right ? width - margin.left - margin.right : width;
-  const xMax = size / 1.5;
-  const yMax = height - margin.bottom - margin.top;
+                                width,
+                                height,
+                                valueOf,
+                            }: HeatmapProps) {
 
-  const binWidth = xMax / binData.length;
+    // The bounds (=area inside the axis) is calculated by substracting the margins
+    const boundsWidth = width - MARGIN.right - MARGIN.left;
+    const boundsHeight = height - MARGIN.top - MARGIN.bottom;
 
-  xScale.range([0, xMax]);
-  yScale.range([yMax, 0]);
+    const allYGroups = useMemo(() => [...new Set(valueOf.map((d) => d.y.toString()))], [valueOf]);
+    const allXGroups = useMemo(() => [...new Set(valueOf.map((d) => d.x.toString()))], [valueOf]);
 
-  return (
-    <svg width={width} height={height}>
-      <rect x={0} y={0} width={width} height={height} fill={background} />
-      <Group top={margin.top} left={margin.left}>
-        <HeatmapRect
-          xScale={(d) => xScale(d) ?? 0}
-          yScale={(d) => yScale(d) ?? 0}
-          colorScale={rectColorScale}
-          binWidth={binWidth}
-          binHeight={binWidth}
-        >
-          {(heatmap) =>
-            heatmap.map((heatmapBins) =>
-              heatmapBins.map((bin) => (
-                <rect
-                  key={`heatmap-rect-${bin.row}-${bin.column}`}
-                  className="visx-heatmap-rect"
-                  width={bin.width}
-                  height={bin.height}
-                  x={bin.x}
-                  y={bin.y}
-                  fill={bin.color}
-                />
-              )),
-            )
-          }
-        </HeatmapRect>
-      </Group>
-    </svg>
-  );
+    const [min = 0, max = 0] = d3.extent(valueOf.map((d) => d.valueOf)); // extent can return [undefined, undefined], default to [0,0] to fix types
+
+    const xScale = useMemo(() => {
+        return d3
+            .scaleBand()
+            .range([0, boundsWidth])
+            .domain(allXGroups)
+            .padding(0.01);
+    }, [valueOf, width]);
+
+    const yScale = useMemo(() => {
+        return d3
+            .scaleBand()
+            .range([boundsHeight, 0])
+            .domain(allYGroups)
+            .padding(0.01);
+    }, [valueOf, height]);
+
+    var colorScale = d3
+        .scaleSequential()
+        .interpolator(d3.interpolateInferno)
+        .domain([min, max]);
+
+    // Build the rectangles
+    const allShapes = valueOf.map((d, i) => {
+        const x = xScale(d.x.toString());
+        const y = yScale(d.y.toString());
+
+        if (d.valueOf === null || !x || !y) {
+            return;
+        }
+
+        return (
+            <rect
+                key={i}
+                r={4}
+                x={xScale(d.x.toString())}
+                y={yScale(d.y.toString())}
+                width={xScale.bandwidth()}
+                height={yScale.bandwidth()}
+                opacity={1}
+                fill={colorScale(d.valueOf)}
+                rx={5}
+                stroke={"white"}
+                cursor="pointer"
+            />
+        );
+    });
+
+    const xLabels = allXGroups.map((name, i) => {
+        const x = xScale(name);
+
+        if (!x) {
+            return null;
+        }
+
+        return (
+            <text
+                key={i}
+                x={x + xScale.bandwidth() / 2}
+                y={boundsHeight + 10}
+                textAnchor="middle"
+                dominantBaseline="middle"
+                fontSize={10}
+            >
+                {name}
+            </text>
+        );
+    });
+
+    const yLabels = allYGroups.map((name, i) => {
+        const y = yScale(name);
+
+        if (!y) {
+            return null;
+        }
+
+        return (
+            <text
+                key={i}
+                x={-5}
+                y={y + yScale.bandwidth() / 2}
+                textAnchor="end"
+                dominantBaseline="middle"
+                fontSize={10}
+            >
+                {name}
+            </text>
+        );
+    });
+
+    return (
+        <svg width={width} height={height}>
+            <g
+                width={boundsWidth}
+                height={boundsHeight}
+                transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+            >
+                {allShapes}
+                {xLabels}
+                {yLabels}
+            </g>
+        </svg>
+    )
 }
--- Tytonic/src/main.tsx
+++ Tytonic/src/main.tsx
@@ -1,13 +1,47 @@
 import React from 'react'
-import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import ReactDOM from 'react-dom/client'
 import App from './app.tsx'
 import './index.css'
+import {getValuesOfHeightmap, addHillToHeightmap, newHeightmap} from "tytonic";
+import {ReadonlyArray2D} from "./array2d.ts";
+
+const WORLD_WIDTH = 80;
+const WORLD_HEIGHT = 50;
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ *
+ * Ref: https://stackoverflow.com/a/1527820
+ */
+function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const heightmap = newHeightmap(WORLD_WIDTH, WORLD_HEIGHT);
+
+for (let i = 0; i < 250; i++) {
+    addHillToHeightmap(heightmap,
+        getRandomInt(WORLD_WIDTH / 10, WORLD_WIDTH - WORLD_WIDTH / 10),
+        getRandomInt(WORLD_HEIGHT / 10, WORLD_HEIGHT - WORLD_HEIGHT / 10),
+        getRandomInt(12, 16),
+        getRandomInt(6, 10)
+    );
+}
+
+const valuesOf = getValuesOfHeightmap(heightmap);
+console.log(valuesOf)
+
+const heightmapOf = new ReadonlyArray2D(valuesOf, WORLD_WIDTH, WORLD_HEIGHT);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ParentSize>
-            {({width, height}) => <App width={width} height={height}/>}
-        </ParentSize>
+       <App valueOf={heightmapOf} width={1400} height={1000}/>
     </React.StrictMode>,
 )
