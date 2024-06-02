51b8e54fae620a1dfd383e2aa591fa5f6862e396
Andres6936 <andres6936@gmail.com>
2024-06-01 19:44:06 -0500

    Refactor index.ts to integrate noise functionality
    The index.ts file under the tests directory has been significantly modified in this commit. The primary change was swapping out the map heightmap functions with the newly introduced noise functions. We continue to keep old code commented out for future comparison or reutilization necessity.

--- Tytonic/tests/index.ts
+++ Tytonic/tests/index.ts
@@ -1,21 +1,28 @@
-import { newHeightmap, addHillToHeightmap, getValuesOfHeightmap } from "../build/debug.js";
-import {ReadonlyArray2D} from "../src/array2d.ts";
-import * as console from "node:console";
+// import { newHeightmap, addHillToHeightmap, getValuesOfHeightmap } from "../build/debug.js";
+// import {ReadonlyArray2D} from "../src/array2d.ts";
 
-const MAP_WIDTH = 3;
-const MAP_HEIGHT = 3;
+import {getBufferNoise, getMapNoise, newNoise} from "../build/debug.js";
 
-const heightmap = newHeightmap(MAP_WIDTH, MAP_HEIGHT);
-// addHillToHeightmap(heightmap, 40, 25, 12, 6);
-const valuesOf = getValuesOfHeightmap(heightmap);
-
-const heightmapOf = new ReadonlyArray2D(valuesOf, MAP_WIDTH, MAP_HEIGHT);
-// for (let heightmapOfElement of heightmapOf) {
-//     console.log(heightmapOfElement)
-// }
+// const MAP_WIDTH = 3;
+// const MAP_HEIGHT = 3;
 //
-// console.log("Using @{map} method of ReadonlyArray2D")
+// const heightmap = newHeightmap(MAP_WIDTH, MAP_HEIGHT);
+// // addHillToHeightmap(heightmap, 40, 25, 12, 6);
+// const valuesOf = getValuesOfHeightmap(heightmap);
+//
+// const heightmapOf = new ReadonlyArray2D(valuesOf, MAP_WIDTH, MAP_HEIGHT);
+// // for (let heightmapOfElement of heightmapOf) {
+// //     console.log(heightmapOfElement)
+// // }
+// //
+// // console.log("Using @{map} method of ReadonlyArray2D")
+//
+// heightmapOf.map((valueOf) => {
+//     console.log(valueOf);
+// })
 
-heightmapOf.map((valueOf) => {
-    console.log(valueOf);
-})
+const noise = newNoise(2, 0.5, 2.0);
+// const buffer = getBufferNoise(noise)
+// console.log(buffer)
+const map = getMapNoise(noise);
+console.log(map)
